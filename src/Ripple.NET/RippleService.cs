d854a48df86c8a2eaf618c2d8ede1491a32b362e
Ivan Savu <ivan.savu@gmail.com>
2026-01-11 22:15:39 +0100

    Removed unnecessary code from RippleService

--- src/Ripple.NET/RippleService.cs
+++ src/Ripple.NET/RippleService.cs
@@ -10,21 +10,6 @@ internal class RippleService : IHostedService
 
     public RippleService(IEnumerable<EndpointDataSource> endpointSources, IHostApplicationLifetime lifetime)
     {
-        lifetime.ApplicationStarted.Register(() =>
-        {
-            lock (apiLock)
-            {
-                foreach (var source in endpointSources)
-                {
-                    foreach (var endpoint in source.Endpoints)
-                    {
-                        if (endpoint.DisplayName == null)
-                            continue;
-                        apiCalls[endpoint.DisplayName] = [];
-                    }
-                }
-            }
-        });
     }
 
     public void Clear()
@@ -36,16 +21,6 @@ internal class RippleService : IHostedService
                 calls.Clear();
             }
         }
-    }
-
-    public Task StartAsync(CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-
-    public Task StopAsync(CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
     }
 
     public void AddAPICall(APICall call)
@@ -68,97 +43,15 @@ internal class RippleService : IHostedService
         }
     }
 
-    private class TransWithParent
+    public Task StartAsync(CancellationToken cancellationToken)
     {
-        private Transaction tx;
-        
-        public TransWithParent(Transaction tx, APICall parent)
-        {
-            this.tx = tx;
-            this.Parent = parent;
-        }
+        return Task.CompletedTask;
+	}
 
-        public APICall Parent { get; private set; }
-        public IReadOnlyCollection<Transaction> Children => tx.Children;
-        public bool ReadOnly => tx.ReadOnly;
-        public IReadOnlyCollection<string> ReadTypes => tx.ReadTypes;
-        public IReadOnlyCollection<string> WriteTypes => tx.WriteTypes;
-    }
+	public Task StopAsync(CancellationToken cancellationToken)
+	{
+		return Task.CompletedTask;
+	}
 
-    private class Operations
-    {
-        public List<TransWithParent> Reads { get; private set;} = [];
-        public List<TransWithParent> Writes { get; private set;} = [];
-    }
-
-    private class Edge
-    {
-        public TransWithParent From { get; private set; }
-        public TransWithParent To { get; private set; }
-
-        public Type Type { get; private set; }
-
-        public bool IsWrite { get; private set; }
-
-        public Edge(TransWithParent from, TransWithParent to, Type type, bool isWrite)
-        {
-            From = from;
-            To = to;
-            Type = type;
-            IsWrite = isWrite;
-        }
-    }
-
-    public void GetGraph()
-    {
-        List<APICall> writeCalls = new();
-        lock (apiLock)
-        {
-            writeCalls = apiCalls.Values.SelectMany(x => x).Where(api => !api.ReadOnly).ToList();
-        }
-
-        var operations = CreateOperations(writeCalls);
-    }
-
-    private static Dictionary<string, Operations> CreateOperations(List<APICall> writeCalls)
-    {
-        Dictionary<string, Operations> operations = new();
-
-        Queue<TransWithParent> queue = new Queue<TransWithParent>(writeCalls.SelectMany(api =>
-                                                api.Transactions.Select(tx => new TransWithParent(tx, api))));
-
-        while (queue.Count > 0)
-        {
-            TransWithParent current = queue.Dequeue();
-
-            foreach (var readType in current.ReadTypes)
-            {
-                if (!operations.TryGetValue(readType, out var ops))
-                {
-                    ops = new Operations();
-                    operations[readType] = ops;
-                }
-                ops.Reads.Add(current);
-            }
-
-            foreach (var writeType in current.WriteTypes)
-            {
-                if (!operations.TryGetValue(writeType, out var ops))
-                {
-                    ops = new Operations();
-                    operations[writeType] = ops;
-                }
-                ops.Writes.Add(current);
-            }
-
-            foreach (var child in current.Children)
-            {
-                queue.Enqueue(new TransWithParent(child, current.Parent));
-            }
-        }
-        return operations;
-    }
-
-    internal Interceptor Interceptor { get; private set; } = new Interceptor();
-
+	internal Interceptor Interceptor { get; private set; } = new Interceptor();
 }
